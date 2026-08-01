96ba342d8f56c5de995f14ad9c136226079def14
Zakhar <Zakhar200915@yandex.ru>
2026-08-01 14:02:57 +0400

    Пофиксил ошибки, расширил окно и добавил смайлики

--- MilligramClient.Api/Clients/Contacts/ContactsClient.cs
+++ MilligramClient.Api/Clients/Contacts/ContactsClient.cs
@@ -1,88 +1,89 @@
 ﻿using MilligramClient.Api.Extensions;
 using MilligramClient.Api.Token;
 using MilligramClient.Domain.Dtos;
-
 using RestSharp;
 
 namespace MilligramClient.Api.Clients.Contacts;
 
 public class ContactsClient : HttpClientBase, IContactsClient
 {
-	private readonly ITokenProvider _tokenProvider;
+    private readonly ITokenProvider _tokenProvider;
 
-	public ContactsClient(ITokenProvider tokenProvider, string address, TimeSpan timeout)
-		: base(address, timeout)
-	{
-		_tokenProvider = tokenProvider;
-	}
+    public ContactsClient(ITokenProvider tokenProvider, string address, TimeSpan timeout)
+        : base(address, timeout)
+    {
+        _tokenProvider = tokenProvider;
+    }
 
-	public Task<ContactDto[]> GetContactsAsync(
-		CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync<ContactDto[]>(
-				Method.Get,
-				"api/contacts",
-				token,
-				cancellationToken));
-	}
+    public Task<ContactDto[]> GetContactsAsync(
+        CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync<ContactDto[]>(
+                Method.Get,
+                "api/contacts",
+                token,
+                cancellationToken));
+    }
 
-	public Task<ContactDto> GetContactAsync(Guid id,
-											CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync<ContactDto>(
-				Method.Get,
-				"api/contacts",
-				token,
-				cancellationToken));
-	}
+    public Task<ContactDto> GetContactAsync(Guid id,
+                                            CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync<ContactDto>(
+                Method.Get,
+                "api/contacts",
+                token,
+                cancellationToken));
+    }
 
-	public Task<ContactDto> CreateContactAsync(ContactDto newContact,
-											   CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync<ContactDto>(
-				Method.Post,
-				"api/contacts",
-				token,
+    public Task<ContactDto> CreateContactAsync(ContactDto newContact,
+                                               CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync<ContactDto>(
+                Method.Post,
+                "api/contacts",
+                token,
                 request => request
                     .AddBody(newContact),
                 cancellationToken));
-	}
+    }
 
-    public Task<ContactDto> UpdateContactAsync(ContactDto updatedContact,
-											   CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync<ContactDto>(
-				Method.Put,
-				"api/contacts",
-				token,
-				cancellationToken));
-	}
+    public Task<ContactDto> UpdateContactAsync(Guid id, ContactDto updatedContact,
+                                               CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync<ContactDto>(
+                Method.Put,
+                $"api/contacts/{id}",
+                token,
+                request => request
+                    .AddBody(updatedContact),
+                cancellationToken));
+    }
 
-	public Task<UserDto[]> SearchUsersAsync(string name,
-											   CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync<UserDto[]>(
-				Method.Get,
+    public Task<UserDto[]> SearchUsersAsync(string name,
+                                               CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync<UserDto[]>(
+                Method.Get,
                 "api/contacts/search",
-				token,
+                token,
                 request => request
                     .AddQueryParameterIfNotNull("name", name),
                 cancellationToken));
-	}
+    }
 
-	public Task DeleteContactsAsync(Guid id,
-									CancellationToken cancellationToken = default)
-	{
-		return _tokenProvider.ExecuteWithToken(token =>
-			SendRequestAsync(
-				Method.Delete,
-				$"api/contacts/{id}",
-				token,
-				cancellationToken));
-	}
+    public Task DeleteContactsAsync(Guid id,
+                                    CancellationToken cancellationToken = default)
+    {
+        return _tokenProvider.ExecuteWithToken(token =>
+            SendRequestAsync(
+                Method.Delete,
+                $"api/contacts/{id}",
+                token,
+                cancellationToken));
+    }
 }
--- MilligramClient.Common/Constants.cs
+++ MilligramClient.Common/Constants.cs
@@ -2,15 +2,9 @@
 
 public static class Constants
 {
-<<<<<<< HEAD
-	public const string ServerAddress = "http://192.168.3.97:5193";
-=======
 	public const string ServerAddress = "http://92.55.7.199:8181";
->>>>>>> master
 	public static readonly TimeSpan RequestTimeout = TimeSpan.FromSeconds(30);
-
 	public const int RefreshSlidingTokenBeforeExpirationInPercent = 50;
-
 	public const string RegistryApplicationName = "MilligramClientAuthorizeExample";
 	public const string RegistrySettingsKey = $@"SOFTWARE\{RegistryApplicationName}";
 }
